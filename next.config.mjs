53a00e10a1f81cfbd836798e28ac9eb465fc380f
Revellison <andrey.akimov2014@gmail.com>
2025-08-09 23:40:26 +0600

    qweк

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: "",
-  assetPrefix: "",
+  basePath: "/Revellison",
+  assetPrefix: "/Revellison",
   trailingSlash: false,
   images: {
     unoptimized: true,
