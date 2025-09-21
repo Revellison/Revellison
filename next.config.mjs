595802296147b75934ea021c7878034b26e4de39
Revellison <andrey.akimov2014@gmail.com>
2025-09-21 21:43:49 +0600

    Подготовка к переходу на домен

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     unoptimized: true,
   },
